59d0e01701ae2cf89832dfcfaf7b993dbfc9266f
Mark <mark@does.work>
2024-05-01 12:30:13 -0400

    Start

--- chapter9_10.rb
+++ chapter9_10.rb
@@ -1,39 +1,38 @@
 require 'minitest/autorun'
 
 class SalesReport
-  def sales 
-    [
+  def run
+    sales = [
       { rep: "Jamie", region: "North East", revenue: "80_000" },
       { rep: "Sam", region: "North East", revenue: "50_000" },
       { rep: "Charlie", region: "Midwest", revenue: "90_000" },
       { rep: "Sam", region: "North East", revenue: "10_000" },
     ]
-  end
 
-  def regional_revenue
-    hash = Hash.new(0)
+    regional_revenue = Hash.new(0)
     for rep in sales
-      hash[rep.fetch(:region)] += rep.fetch(:revenue).to_i
+      regional_revenue[rep.fetch(:region)] += rep.fetch(:revenue).to_i
     end
 
-    hash 
+    regional_revenue
   end
 end
 
 class SalesReportTest < Minitest::Test
   def test_run
     expected = {"North East"=>140000, "Midwest"=>90000}
-    actual = SalesReport.new.regional_revenue
+    actual = SalesReport.new.run
 
     assert_equal expected, actual
   end
 end
 
-# >> Run options: --seed 62071
-# >> 
+# >> Run options: --seed 12117
+# >>
 # >> # Running:
-# >> 
+# >>
 # >> .
-# >> 
-# >> Finished in 0.000364s, 2747.2527 runs/s, 2747.2527 assertions/s.
+# >>
+# >> Finished in 0.000326s, 3067.4849 runs/s, 3067.4849 assertions/s.
+# >>
 # >> 1 runs, 1 assertions, 0 failures, 0 errors, 0 skips

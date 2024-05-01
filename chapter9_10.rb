d63db3c19e914b44988f2a0530e21e46cfd7729a
Mark <mark@does.work>
2024-05-01 12:30:45 -0400

    Change reference to value

--- chapter9_10.rb
+++ chapter9_10.rb
@@ -1,38 +1,39 @@
 require 'minitest/autorun'
 
 class SalesReport
-  def run
-    sales = [
+  def sales 
+    [
       { rep: "Jamie", region: "North East", revenue: "80_000" },
       { rep: "Sam", region: "North East", revenue: "50_000" },
       { rep: "Charlie", region: "Midwest", revenue: "90_000" },
       { rep: "Sam", region: "North East", revenue: "10_000" },
     ]
+  end
 
-    regional_revenue = Hash.new(0)
+  def regional_revenue
+    hash = Hash.new(0)
     for rep in sales
-      regional_revenue[rep.fetch(:region)] += rep.fetch(:revenue).to_i
+      hash[rep.fetch(:region)] += rep.fetch(:revenue).to_i
     end
 
-    regional_revenue
+    hash 
   end
 end
 
 class SalesReportTest < Minitest::Test
   def test_run
     expected = {"North East"=>140000, "Midwest"=>90000}
-    actual = SalesReport.new.run
+    actual = SalesReport.new.regional_revenue
 
     assert_equal expected, actual
   end
 end
 
-# >> Run options: --seed 12117
-# >>
+# >> Run options: --seed 62071
+# >> 
 # >> # Running:
-# >>
+# >> 
 # >> .
-# >>
-# >> Finished in 0.000326s, 3067.4849 runs/s, 3067.4849 assertions/s.
-# >>
+# >> 
+# >> Finished in 0.000364s, 2747.2527 runs/s, 2747.2527 assertions/s.
 # >> 1 runs, 1 assertions, 0 failures, 0 errors, 0 skips
